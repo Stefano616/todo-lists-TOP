cbd4ff2876dd2690ca8937edf7eefcf3576a11e1
Stefano Rocchetti <stefanorocchetti4@gmail.com>
2025-02-07 20:57:15 +0100

    webpack setup

---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const { merge } = require("webpack-merge");
+const webpackCommon = require("./webpack.common");
+
+module.exports = merge(webpackCommon, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+});
